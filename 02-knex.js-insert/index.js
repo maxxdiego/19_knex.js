a50cac2848f5979a945f53a487485e24ebdf4ca6
maxx.diego <77diego.max@gmail.com>
2024-11-12 16:13:47 -0300

    @maxxdiego

--- 02-knex.js-insert/index.js
+++ 02-knex.js-insert/index.js
@@ -1,3 +1,35 @@
 const database = require("./database");
 
+// INSERT (Inserindo dados no banco)
 
+// const dados = {
+//   nome: "Sea of Thieves",
+//   preco: 50.67,
+// };
+
+const dados = [
+  {
+    nome: "Call of Dutty 2",
+    preco: 60,
+  },
+  {
+    nome: "GTA",
+    preco: 40,
+  },
+  {
+    nome: "WoW",
+    preco: 120,
+  },
+];
+
+const query = database
+  .insert(dados)
+  .into("games")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// console.log(query.toQuery());
