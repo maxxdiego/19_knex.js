eac46e7bbe618105bf7b9016651cad9aad15cb90
maxx.diego <77diego.max@gmail.com>
2024-11-12 16:51:52 -0300

    @maxxdiego

---
+++ 04-knex.js-where/index.js
@@ -0,0 +1,30 @@
+const database = require("./database");
+
+// WHERE
+// const query = database
+//   .select(["nome", "preco"])
+//   .where({ nome: "GTA" })
+//   // .where({ id: "4" })
+//   .orWhere({id: 2})
+//   .table("games");
+// console.log(query.toQuery());
+
+
+// WHERE RAW
+// const query = database
+//   .select(["nome", "preco"])
+//   // .where({ nome: "GTA" })
+//   .whereRaw("nome = 'GTA' OR preco > 50")
+//   .table("games");
+// console.log(query.toQuery());
+
+// MOSTRANDO OS DADOS
+const query = database
+  .select(["nome", "preco"])
+  // .where({ nome: "GTA" })
+  .whereRaw("nome = 'GTA' OR preco > 50")
+  .table("games").then(data => {
+    console.log(data)
+  }).catch(error => {
+    console.log(error)
+  })
